b062c799911c795df047ba3bfb35c5041ecec0de
Martin Frehner <mfrehner79@gmail.com>
2019-08-15 20:47:33 +0200

    Add Equals

--- PetManager/PetManager/Pet.cs
+++ PetManager/PetManager/Pet.cs
@@ -17,7 +17,8 @@ namespace PetManager {
             {
                 int hash = 17;
                 hash = hash * 23 + (this.Name != null ? this.Name.GetHashCode() : 0);
-                hash = hash * 23 + (this.Breed != null ? this.Breed.GetHashCode() : 0);              
+                hash = hash * 23 + (this.Breed != null ? this.Breed.GetHashCode() : 0);
+                hash = hash * 23 + (this.Birthday != null ? this.Birthday.GetHashCode() : 0); 
                 return hash;
             }
         }
@@ -44,5 +45,18 @@ namespace PetManager {
         {
             Birthday = DateTime.Now.Date;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Pet))
+            {
+                return object.Equals(obj, this);
+            }
+
+            var pet = (Pet)obj;
+            return string.Equals(this.Name, pet.Name) && Birthday.Equals(pet.Birthday) && 
+                   string.Equals(this.Breed, pet.Breed);
+        }
+
     }
 }
